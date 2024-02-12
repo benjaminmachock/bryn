39e5ad6ccb6d37e4a91546e68f6ccf86658801d6
Benjamin Machock <bmachock@outlook.com>
2024-02-12 10:59:10 -0700

    test

--- server/server.js
+++ server/server.js
@@ -7,7 +7,6 @@ const { ApolloServer } = require("@apollo/server");
 const { expressMiddleware } = require("@apollo/server/express4");
 const { authMiddleware } = require("./utils/auth");
 
-const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
@@ -15,20 +14,23 @@ const server = new ApolloServer({
   context: authMiddleware,
 }); // here
 
+const app = express();
+
 const startApolloServer = async () => {
   await server.start();
 
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
+  app.use("/graphql", expressMiddleware(server));
+
   if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/build")));
+    app.use(express.static(path.join(__dirname, "../client/dist")));
+
     app.get("*", (req, res) => {
       res.sendFile(path.join(__dirname, "../client/dist/index.html"));
     });
   } //here
-
-  app.use("/graphql", expressMiddleware(server));
 
   db.once("open", () => {
     app.listen(PORT, () => {
